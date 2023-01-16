fbd2a76dbe952a694237e9bf1e998fab1293a657
Nathan Henrie <nate@n8henrie.com>
2023-01-16 06:17:44 -0700

    Baseline

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
+#[cfg(feature = "foo")]
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
